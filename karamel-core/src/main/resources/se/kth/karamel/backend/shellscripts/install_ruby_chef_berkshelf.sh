1aa251cc8107e458fd9d1b78f4b3a3b6257bd6e7
Jim Dowling <jdowling@sics.se>
2015-03-18 17:17:50 +0100

    Download chefdk from source, not snurran

--- karamel-core/src/main/resources/se/kth/karamel/backend/shellscripts/install_ruby_chef_berkshelf.sh
+++ karamel-core/src/main/resources/se/kth/karamel/backend/shellscripts/install_ruby_chef_berkshelf.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
+BINARIES=https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chefdk_0.4.0-1_amd64.deb
+CHEFDK=`echo $BINARIES | sed 's/.*\///'`
+
 if [ -f chefdk_0.1.0-1_amd64-ubuntu12.deb ] ; then
-  sudo dpkg -i chefdk_0.1.0-1_amd64-ubuntu12.deb
+  sudo dpkg -i $CHEFDK
   if [ $? -ne 0 ] ; then
-      rm chefdk_0.1.0-1_amd64-ubuntu12.deb
-      wget http://snurran.sics.se/hops/chefdk_0.1.0-1_amd64-ubuntu12.deb
-      sudo dpkg -i chefdk_0.1.0-1_amd64-ubuntu12.deb
+      rm $CHEFDK
+      wget $BINARIES
+      sudo dpkg -i $CHEFDK
   fi
-else 
-  wget http://snurran.sics.se/hops/chefdk_0.1.0-1_amd64-ubuntu12.deb
-  sudo dpkg -i chefdk_0.1.0-1_amd64-ubuntu12.deb
+else
+  wget $BINARIES
+  sudo dpkg -i $CHEFDK
+fi
+if [ $? -ne 0 ] ; then
+  exit 1
 fi
 
 exit 0
