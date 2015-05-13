c87e4f0c23da16d8e431fde64e9a041acd5080e6
Kamal Hakimzadeh <kamal@sics.se>
2015-05-13 18:09:24 +0200

    Visual Dag: Some workaraound for d3 positioning by manipulatiing the DOM, to be fixed in future. It closes #42

--- karamel-core/src/main/java/se/kth/karamel/backend/dag/Dag.java
+++ karamel-core/src/main/java/se/kth/karamel/backend/dag/Dag.java
@@ -5,6 +5,7 @@
  */
 package se.kth.karamel.backend.dag;
 
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -94,7 +95,7 @@ public class Dag {
     for (DagNode node : allNodes.values()) {
       if (node.getTask() == null) {
         throw new DagConstructionException(String.format("No task assigned to '%s' while it appreard in dependencies.. "
-                + "predecessors: %s successors: %s", node.getId(), node.getPredecessors().toString(), node.getSuccessors().toString()));
+            + "predecessors: %s successors: %s", node.getId(), node.getPredecessors().toString(), node.getSuccessors().toString()));
       }
     }
   }
@@ -145,8 +146,23 @@ public class Dag {
         return true;
       }
     }
-
     return false;
+  }
+
+  public String asJson() {
+    StringBuilder builder = new StringBuilder();
+    builder.append("[");
+    Collection<DagNode> values = allNodes.values();
+    int i = 0;
+    for (DagNode dagNode : values) {
+      i++;
+      builder.append(dagNode.toJson());
+      if (i != allNodes.size()) {
+        builder.append(",");
+      }
+    }
+    builder.append("]");
+    return builder.toString();
   }
 
 }
