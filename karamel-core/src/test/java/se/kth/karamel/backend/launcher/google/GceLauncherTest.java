ced149ea13c0f000cb3655c6ff1dcb914d4446ca
Hooman Peiro Sajjad <shps@kth.se>
2015-05-22 14:51:34 +0200

    Added an empty test class. This commit is just to test repository access! Not any other added value.

---
+++ karamel-core/src/test/java/se/kth/karamel/backend/launcher/google/GceLauncherTest.java
@@ -0,0 +1,28 @@
+package se.kth.karamel.backend.launcher.google;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author Hooman
+ */
+public class GceLauncherTest {
+
+    public GceLauncherTest() {
+    }
+
+    /**
+     * Test of readCredentials method, of class GceLauncher.
+     */
+    @Test
+    public void testReadCredentials() {
+    }
+
+    /**
+     * Test of createSecurityGroup method, of class GceLauncher.
+     */
+    @Test
+    public void testCreateSecurityGroup() throws Exception {
+    }
+
+}
