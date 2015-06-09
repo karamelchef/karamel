35cf54becf3fb028c163a098d5a05e5933586a03
Jim Dowling <jdowling@kth.se>
2015-06-09 07:42:55 +0200

    install chef-dk script is now one line - more robust if failure during download

--- karamel-core/src/main/resources/se/kth/karamel/backend/shellscripts/install_ruby_chef_berkshelf.sh
+++ karamel-core/src/main/resources/se/kth/karamel/backend/shellscripts/install_ruby_chef_berkshelf.sh
@@ -1,2 +1,1 @@
-rm chefdk_0.4.0-1_amd64.deb -f
-wget https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chefdk_0.4.0-1_amd64.deb && %sudo_command% dpkg -i chefdk_0.4.0-1_amd64.deb
+rm chefdk_0.4.0-1_amd64.deb -f && wget https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chefdk_0.4.0-1_amd64.deb && %sudo_command% dpkg -i chefdk_0.4.0-1_amd64.deb
