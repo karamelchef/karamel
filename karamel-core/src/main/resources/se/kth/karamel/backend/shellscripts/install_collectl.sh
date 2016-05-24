362f0fd4ae8c17e5c81df889130f370534d20a74
stornqvist <basti94a@gmail.com>
2016-05-24 11:09:06 +0200

    updated collectl version

--- karamel-core/src/main/resources/se/kth/karamel/backend/shellscripts/install_collectl.sh
+++ karamel-core/src/main/resources/se/kth/karamel/backend/shellscripts/install_collectl.sh
@@ -1,4 +1,7 @@
 echo $$ > %pid_file%; echo '#!/bin/bash
-%sudo_command% apt-get install collectl -y
+%sudo_command% wget http://sourceforge.net/projects/collectl/files/collectl/collectl-4.0.4/collectl-4.0.4.src.tar.gz
+%sudo_command% tar -xvzf collectl-4.0.4.src.tar.gz
+%sudo_command% cd collectl-4.0.4
+%sudo_command% ./INSTALL
 echo '%task_id%' >> %succeedtasks_filepath%
 ' > collectl-install.sh ; chmod +x collectl-install.sh ; ./collectl-install.sh
