246318720db75ff6dc710a9b09e648a0c7e3caa1
ashansa <ashansa.perera@gmail.com>
2016-06-03 08:10:49 +0200

    adding a temporary method to send vmInfo to autoscalar

--- karamel-core/src/main/java/se/kth/karamel/backend/honeytap/HoneyTapHandler.java
+++ karamel-core/src/main/java/se/kth/karamel/backend/honeytap/HoneyTapHandler.java
@@ -3,14 +3,18 @@ package se.kth.karamel.backend.honeytap;
 import org.apache.log4j.Logger;
 import se.kth.honeytap.scaling.ScalingSuggestion;
 import se.kth.honeytap.scaling.core.HoneyTapAPI;
+import se.kth.honeytap.scaling.exceptions.HoneyTapException;
 import se.kth.honeytap.scaling.models.MachineType;
 //import se.kth.honeytap.scaling.rules.Rule;
 import se.kth.karamel.backend.ClusterService;
 import se.kth.karamel.backend.running.model.GroupRuntime;
+import se.kth.karamel.backend.running.model.MachineRuntime;
 import se.kth.karamel.common.exception.KaramelException;
+import se.kth.karamel.common.launcher.amazon.InstanceType;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.Executors;
@@ -96,14 +100,17 @@ public class HoneyTapHandler {
             case SCALE_IN:
               ArrayList<String> machinesToRemove = suggestion.getScaleInSuggestions();
               handleScaleInSuggestion(machinesToRemove.toArray(new String[machinesToRemove.size()]));
+              resetVmInfoAtMonitor(groupRuntime.getId());
               break;
             case SCALE_OUT:
               ArrayList<MachineType> scaleOutMachines = suggestion.getScaleOutSuggestions();
               handleScaleOutSuggestion(scaleOutMachines.toArray(new MachineType[scaleOutMachines.size()]));
+              resetVmInfoAtMonitor(groupRuntime.getId());
               break;
             case TMP_SCALEIN:
               ArrayList<String> toRemove = suggestion.getScaleInSuggestions();
               handleScaleInSuggestion(toRemove.toArray(new String[toRemove.size()]));
+              resetVmInfoAtMonitor(groupRuntime.getId());
               break;
             default:
               logger.warn("Handle scaling has not been implemented for the scaling direction: " +
@@ -122,8 +129,8 @@ public class HoneyTapHandler {
 
     private void handleScaleInSuggestion(String[] vmIds) {
       try {
-        //TODO-AS temporary removing first Id -remove passing ids
-        vmIds = new String[]{groupRuntime.getMachines().get(0).getVmId()};
+        /*//TODO-AS temporary removing first Id -remove passing ids
+        vmIds = new String[]{groupRuntime.getMachines().get(0).getVmId()};*/
 
         clusterService.scaleInClusterGroup(groupRuntime.getCluster().getName(), groupRuntime.getName(), vmIds);
       } catch (KaramelException e) {
@@ -138,6 +145,24 @@ public class HoneyTapHandler {
       } catch (KaramelException e) {
         logger.error("Failed to scale in the group: " + groupRuntime.getName() + " of cluster: " +
                 groupRuntime.getCluster().getName());
+      }
+    }
+
+    private void resetVmInfoAtMonitor(String groupId) {
+      List<MachineRuntime> machineRuntimes = groupRuntime.getMachines();
+      for (int i = 0; i < machineRuntimes.size(); ++i) {
+        MachineRuntime machineRuntime = machineRuntimes.get(i);
+        boolean isStart = false;
+        if (i == 0) {
+          isStart = true;
+        }
+        try {
+          InstanceType instanceType = InstanceType.valueByModel(machineRuntime.getMachineType().split("/")[2]);
+          HoneyTapAPI.getInstance().addVmInfo(groupId, machineRuntime.getVmId(), instanceType.numVCpu,
+                  instanceType.memInGig, instanceType.numDisks, instanceType.diskSize, isStart);
+        } catch (HoneyTapException e) {
+          throw new IllegalStateException(e);
+        }
       }
     }
 
