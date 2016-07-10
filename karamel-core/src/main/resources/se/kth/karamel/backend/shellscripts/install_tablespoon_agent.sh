0c81c6e58a36c1360edc7f46095904a79f9f05b4
Sebastian Törnqvist <basti94a@gmail.com>
2016-07-10 19:55:59 +0200

    updated tablespoon-agent version

--- karamel-core/src/main/resources/se/kth/karamel/backend/shellscripts/install_tablespoon_agent.sh
+++ karamel-core/src/main/resources/se/kth/karamel/backend/shellscripts/install_tablespoon_agent.sh
@@ -1,6 +1,7 @@
 echo $$ > %pid_file%; echo '#!/bin/bash
-%sudo_command% mkdir /usr/local/tablespoon-agent
-%sudo_command% wget -O /usr/local/tablespoon-agent/tablespoon-agent.jar "https://www.dropbox.com/s/klmxlg10pme3f1q/tablespoon-agent.jar"
+%sudo_command% wget -O /usr/local/tablespoon-agent.tar.gz "https://www.dropbox.com/s/byrn8h1bjnrg18g/tablespoon-agent.tar.gz"
+%sudo_command% cd /usr/local
+%sudo_command% tar -zxvf tablespoon-agent.tar.gz
 %sudo_command% wget -O /etc/init.d/tablespoon-agent "https://www.dropbox.com/s/xunhnu04lvmaxab/tablespoon-agent"
 %sudo_command% chmod +x /etc/init.d/tablespoon-agent
 echo '%task_id%' >> %succeedtasks_filepath%
