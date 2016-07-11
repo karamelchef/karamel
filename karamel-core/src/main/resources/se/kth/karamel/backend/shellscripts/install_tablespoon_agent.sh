bc3d56f2c91ad17000bac091406867e380c069e3
Sebastian Törnqvist <basti94a@gmail.com>
2016-07-11 11:32:29 +0200

    small fix

--- karamel-core/src/main/resources/se/kth/karamel/backend/shellscripts/install_tablespoon_agent.sh
+++ karamel-core/src/main/resources/se/kth/karamel/backend/shellscripts/install_tablespoon_agent.sh
@@ -1,6 +1,6 @@
 echo $$ > %pid_file%; echo '#!/bin/bash
 %sudo_command% wget -O /usr/local/tablespoon-agent.tar.gz "https://www.dropbox.com/s/byrn8h1bjnrg18g/tablespoon-agent.tar.gz"
-%sudo_command% cd /usr/local
+cd /usr/local
 %sudo_command% tar -zxvf tablespoon-agent.tar.gz
 %sudo_command% wget -O /etc/init.d/tablespoon-agent "https://www.dropbox.com/s/xunhnu04lvmaxab/tablespoon-agent"
 %sudo_command% chmod +x /etc/init.d/tablespoon-agent
