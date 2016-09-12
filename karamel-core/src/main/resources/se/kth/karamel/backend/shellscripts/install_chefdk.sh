0b43defb878771b42604173e340f9cc4ee9766e0
Kamal Hakimzadeh <k.hakimzadeh@gmail.com>
2016-09-12 13:20:47 +0200

    why this left out

--- karamel-core/src/main/resources/se/kth/karamel/backend/shellscripts/install_chefdk.sh
+++ karamel-core/src/main/resources/se/kth/karamel/backend/shellscripts/install_chefdk.sh
@@ -1,34 +1,30 @@
 echo $$ > %pid_file%; echo '#!/bin/bash
 
 if [ %osfamily% == "redhat" ] ; then
-if [ -f chefdk-0.8.0-1.el6.x86_64.rpm ] ; then
-  checksum=`sha256sum chefdk-0.8.0-1.el6.x86_64.rpm | grep -o '^\S\+'`
-  if [ "$checksum" != "554b087a78e66bab17703ffef1d688dd86eeb7b5ea00b65dc2ca07f9f0ab0077" ] ; then
-      rm -f chefdk-0.8.0-1.el6.x86_64.rpm 
-      wget https://opscode-omnibus-packages.s3.amazonaws.com/el/6/x86_64/chefdk-0.8.0-1.el6.x86_64.rpm 
+  chefdkfile='chefdk-%chefdk_version%-1.el7.x86_64.rpm'
+
+  if [ -f $chefdkfile ] ; then
+    rm -f "$chefdkfile"
+  else
+    wget "http://snurran.sics.se/hops/$chefdkfile"
   fi
-else
-   wget https://opscode-omnibus-packages.s3.amazonaws.com/el/6/x86_64/chefdk-0.8.0-1.el6.x86_64.rpm 
-fi
-%sudo_command% yum install -y chefdk-0.8.0-1.el6.x86_64.rpm 
-echo '%task_id%' >> %succeedtasks_filepath%
+
+  %sudo_command% yum install -y "$chefdkfile" && echo '%task_id%' >> %succeedtasks_filepath%
 
 elif [ %osfamily% == "ubuntu" ] ; then
+  chefdkfile='chefdk_%chefdk_version%-1_amd64.deb'
 
-if [ -f chefdk_0.6.2-1_amd64.deb ] ; then
-  checksum=`sha256sum chefdk_0.6.2-1_amd64.deb | grep -o '^\S\+'`
-  if [ "$checksum" != "e6990a3821590b999d09b707ff124fa891d44849cd63fd4f6f9db73bf5cb23f9" ] ; then
-      rm -f chefdk_0.6.2-1_amd64.deb
-      wget https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chefdk_0.6.2-1_amd64.deb
+  if [ -f $chefdkfile ] ; then
+    rm -f "$chefdkfile"
+  else
+    wget "http://snurran.sics.se/hops/$chefdkfile"
   fi
-else
-   wget https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chefdk_0.6.2-1_amd64.deb
-fi
-%sudo_command% dpkg -i chefdk_0.6.2-1_amd64.deb && echo '%task_id%' >> %succeedtasks_filepath%
-echo \"Found ubuntu\"
+
+  %sudo_command% dpkg -i "$chefdkfile" && echo '%task_id%' >> %succeedtasks_filepath%
+  echo \"Found ubuntu\"
 else 
  echo "Unrecognized version of linux. Not ubuntu or redhat family."
  exit 1
 fi
 echo '%task_id%' >> ~/%succeedtasks_filepath%
-' > berks-install.sh ; chmod +x berks-install.sh ; ./berks-install.sh
+' > install-chefdk.sh ; chmod +x install-chefdk.sh ; ./install-chefdk.sh
