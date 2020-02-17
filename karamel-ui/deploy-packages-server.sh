a81e745115831f27aa90c26a2ede50c57fc91b56
Jim Dowling <jdowling@sics.se>
2020-02-17 23:30:55 +0100

    Add support for http proxy to karamel: set env variables  and

--- karamel-ui/deploy-packages-server.sh
+++ karamel-ui/deploy-packages-server.sh
@@ -9,6 +9,29 @@ if [ ! -d ../../karamel-chef ] ; then
   exit 1
 fi
 
+karamelize() {
+
+    cd appassembler/bin
+    head -n -9 karamel > karamel.tmp
+    echo '
+exec "$JAVACMD" $JAVA_OPTS -Xms128m \
+  -classpath "$CLASSPATH" \
+  -Dapp.name="karamel" \
+  -Dapp.pid="$$" \
+  -Dapp.repo="$REPO" \
+  -Dapp.home="$BASEDIR" \
+  -Dhttp.proxy="$http_proxy"\
+  -Dhttps.proxy="$https_proxy"\
+  -Dbasedir="$BASEDIR" \
+  se.kth.karamel.webservice.KaramelServiceApplication \
+  -server conf/dropwizard.yml "$@"
+' >> karamel.tmp
+    mv -f karamel.tmp karamel
+    chmod +x karamel
+    cd ../..
+}    
+
+
 # This reads the pom.xml file in the current directory, and extracts the first version element in the xml version element.
 version=$(mvn -Dexec.executable='echo' -Dexec.args='${project.version}' --non-recursive exec:exec -q)
 
@@ -22,7 +45,7 @@ cd ../karamel-chef
 git pull
 cd ../karamel
 cd karamel-ui/target
-
+karamelize
 #create linux archive
 cp -r appassembler/* $dist/
 cp ../README.linux $dist/README.txt
@@ -34,11 +57,13 @@ mv $dist ${dist}-linux
 #create jar archive
 mkdir ${dist}-jar
 cp karamel-ui-${version}-shaded.jar ${dist}-jar/karamel-ui-${version}.jar
+
 cp -r appassembler/conf/* ${dist}-jar/
 cp ../README.linux ${dist}-jar/README.txt
 mkdir ${dist}-jar/examples
 cp ../../../karamel-chef/cluster-defns/* ${dist}-jar/examples/
 zip -r ${dist}-jar.zip $dist-jar
+
 
 scp ${dist}.tgz glassfish@snurran.sics.se:/var/www/karamel.io/sites/default/files/downloads/
 scp ${dist}-jar.zip glassfish@snurran.sics.se:/var/www/karamel.io/sites/default/files/downloads/
